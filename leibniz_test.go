fbc0280a7b4373c85adc58017054eb155f735170
Kyle Luzny <kluzny@gmail.com>
2016-11-15 00:52:43 -0600

    removed longest test, i mean 10,000,000 is a lot

--- leibniz_test.go
+++ leibniz_test.go
@@ -8,12 +8,11 @@ func benchmarkLeibnizπ(i int, b *testing.B) {
 	}
 }
 
-func BenchmarkLeibnizπ1(b *testing.B)         { benchmarkLeibnizπ(1, b) }
-func BenchmarkLeibnizπ10(b *testing.B)        { benchmarkLeibnizπ(10, b) }
-func BenchmarkLeibnizπ100(b *testing.B)       { benchmarkLeibnizπ(100, b) }
-func BenchmarkLeibnizπ1000(b *testing.B)      { benchmarkLeibnizπ(1000, b) }
-func BenchmarkLeibnizπ10000(b *testing.B)     { benchmarkLeibnizπ(10000, b) }
-func BenchmarkLeibnizπ100000(b *testing.B)    { benchmarkLeibnizπ(100000, b) }
-func BenchmarkLeibnizπ1000000(b *testing.B)   { benchmarkLeibnizπ(1000000, b) }
-func BenchmarkLeibnizπ10000000(b *testing.B)  { benchmarkLeibnizπ(10000000, b) }
-func BenchmarkLeibnizπ100000000(b *testing.B) { benchmarkLeibnizπ(100000000, b) }
+func BenchmarkLeibnizπ1(b *testing.B)        { benchmarkLeibnizπ(1, b) }
+func BenchmarkLeibnizπ10(b *testing.B)       { benchmarkLeibnizπ(10, b) }
+func BenchmarkLeibnizπ100(b *testing.B)      { benchmarkLeibnizπ(100, b) }
+func BenchmarkLeibnizπ1000(b *testing.B)     { benchmarkLeibnizπ(1000, b) }
+func BenchmarkLeibnizπ10000(b *testing.B)    { benchmarkLeibnizπ(10000, b) }
+func BenchmarkLeibnizπ100000(b *testing.B)   { benchmarkLeibnizπ(100000, b) }
+func BenchmarkLeibnizπ1000000(b *testing.B)  { benchmarkLeibnizπ(1000000, b) }
+func BenchmarkLeibnizπ10000000(b *testing.B) { benchmarkLeibnizπ(10000000, b) }

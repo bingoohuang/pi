44d600679b468daaf9304429cfb61ee7cd4256b7
bingoohuang <bingoo.huang@gmail.com>
2023-01-12 22:37:23 +0800

    iter flag

--- pi.go
+++ pi.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math"
+	"time"
 )
 
 func main() {
-	var iter float64 = 1000000
-	fmt.Printf("Estimating π using %.0f iterations\n", iter)
-	π := leibnizπ(iter)
+	iter := flag.Float64("iter", 10000000, "iterations")
+	flag.Parse()
+
+	fmt.Printf("Estimating π using %.0f iterations\n", *iter)
+	start := time.Now()
+	π := leibnizπ(*iter)
+	cost := time.Since(start)
 	absoluteError, relativeError, percentError := errorCalculation(π)
-	fmt.Println("π:              ", π)
-	fmt.Println("math.Pi:        ", math.Pi)
-	fmt.Println("Absolute Error: ", absoluteError)
-	fmt.Println("Relative Error: ", relativeError)
-	fmt.Println("Percent Error:  ", percentError, "%")
+	fmt.Println("cost           :", cost)
+	fmt.Println("π              :", π)
+	fmt.Println("math.Pi        :", math.Pi)
+	fmt.Println("Absolute Error :", absoluteError)
+	fmt.Println("Relative Error :", relativeError)
+	fmt.Println("Percent Error  :", percentError, "%")
 }
 
 func errorCalculation(π float64) (float64, float64, float64) {
